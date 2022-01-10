dc3b2253e0f4e0a1db3bb4f28ef939f9e8dc41db
jk <378995953@qq.com>
2022-01-10 15:19:18 +0800

    1

--- lodash/jk378-lodash.js
+++ lodash/jk378-lodash.js
@@ -1,6 +1,18 @@
-jk378 = {
-  chunk : function (){
-
+var jk378 = {
+  chunk : function chunk(array, size = 1){
+    var res = []
+    var count = 0
+    var temp = []
+    for(var i = 0; i < array.length;i++){
+      temp.push(array[i])
+      count++
+      if(count == size || i == array.length - 1){
+        res.push(temp)
+        temp = []
+        count = 0
+      }
+    }
+    return res
   }
 
 
